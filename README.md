22093e8d0b95bbb4212ccc2f6dc40482e3080d72
Rômulo do Carmo Sousa <carmosousa20@outlook.com>
2020-09-16 10:56:44 -0400

    Template do projeto

---
+++ README.md
@@ -0,0 +1,3 @@
+# Projeto em Flask
+
+Projeto do do livro [Flask de A a Z](https://www.casadocodigo.com.br/products/livro-flask-a-z?_pos=2&_sid=df5bec62a&_ss=r)
---
+++ config.py
@@ -0,0 +1,46 @@
+import os
+import random
+import string
+
+
+class Config:
+    CSRF_ENABLED = True
+    SECRET = r'ysb_92=qe#djf8%ng+a*#4rt#5%3*4k5%i2bck*gn@w3@f&-&'
+    TEMPLATE_FOLDER = os.path.join(os.path.dirname(
+        os.path.abspath(__file__)), 'templates')
+    ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+    APP = None
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:199718@172.17.0.1:3306/livro_flask'
+
+
+class DeveloperConfig(Config):
+    TESTING = True
+    DEBUG = True
+    IP_HOST = 'localhost'
+    PORT_HOST = 8000
+    URL_MAIN = f'http://{IP_HOST}:{PORT_HOST}'
+
+
+class TestingConfig(Config):
+    TESTING = True
+    DEBUG = True
+    IP_HOST = 'localhost'
+    PORT_HOST = 5000
+    URL_MAIN = f'http://{IP_HOST}:{PORT_HOST}'
+
+
+class ProductConfig(Config):
+    TESTING = False
+    DEBUG = False
+    IP_HOST = 'localhost'
+    PORT_HOST = 8080
+    URL_MAIN = f'http://{IP_HOST}:{PORT_HOST}'
+
+
+app_config = {
+    'development': DeveloperConfig(),
+    'testing': TestingConfig(),
+    'production': ProductConfig()
+}
+
+app_active = os.getenv('FLASK_ENV')

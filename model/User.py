6ace1ae7bf94fcc96eb00ba5530f0149403a5944
Rômulo do Carmo Sousa <carmosousa20@outlook.com>
2020-09-19 13:39:00 -0400

    Adicionando o model do usuário

--- model/User.py
+++ model/User.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from flask_sqlalchemy import SQLAlchemy
+from passlib.hash import pbkdf2_sha256
 
 from config import app_active, app_config
 from model.Role import Role
@@ -17,15 +18,37 @@ class User(db.Model):
     password = db.Column(db.String(80), nullable=False)
 
     date_created = db.Column(
-        db.DateTime(6),
-        default=db.func.current_timestamp(),
-        nullable=False)
-
+        db.DateTime(6), default=db.func.current_timestamp(), nullable=False
+    )
     last_update = db.Column(
-        db.DateTime(6),
-        onupdate=db.func.current_timestamp(),
-        nullable=True
+        db.DateTime(6), onupdate=db.func.current_timestamp(), nullable=True
     )
     recovery_code = db.Column(db.String(200), nullable=True)
     active = db.Column(db.Boolean(), default=1, nullable=True)
     role = db.Column(db.Integer, db.ForeignKey(Role.id), nullable=False)
+
+    def get_user_by_email(self):
+        return ""
+
+    def get_user_by_id(self):
+        return ""
+
+    def update(self, obj):
+        pass
+
+    def hash_password(self, password):
+        try:
+            return pbkdf2_sha256.hash(password)
+
+        except Exception as e:
+            print(f"Erro ao criptografar senha {e}")
+
+    def set_password(self, password):
+        self.password = pbkdf2_sha256.hash(password)
+
+    def verify_password(self, password_no_hash, password_database):
+        try:
+            pbkdf2_sha256.verify(password_no_hash, password_database)
+
+        except ValueError:
+            return False

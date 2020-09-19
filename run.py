2aa1b067ae14a46f2c16e195044357de2738f0bb
Rômulo do Carmo Sousa <carmosousa20@outlook.com>
2020-09-19 13:40:54 -0400

    Modificando o run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import sys
+# from importlib import reload
 
 from app import create_app
 from config import app_active, app_config
@@ -8,4 +9,4 @@ config.APP = create_app(app_active)
 
 if __name__ == '__main__':
     config.APP.run(host=config.IP_HOST, port=config.PORT_HOST)
-    reload(sys)
+    # reload(sys)
